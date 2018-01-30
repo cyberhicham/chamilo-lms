afadaa559f3b2f9bf64aa52e9961eed15a2038b7
jmontoyaa <gugli100@gmail.com>
2018-01-30 14:36:08 +0100

    Minor - Remove unused code

--- l.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'main/inc/global.inc.php';
-$result = UserManager::logInAsFirstAdmin();
-if ($result) {
-    header('Location: index.php');
-    exit;
-}
-api_not_allowed();
-exit;
